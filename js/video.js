7272e225453bcb122853c8eaf7363feae58b9ac8
Oksana Saienko <saienko.oksana.p@gmail.com>
2024-07-21 15:25:48 +0300

    Added 4,5 section

---
+++ js/video.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var playButton = document.getElementById('playButton');
+    var video = document.getElementById('myVideo');
+        playButton.addEventListener('click', function() {
+             video.play();
+    });
+  });
+
+  
+
+
